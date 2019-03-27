a054913cbbf17252dc515804a9c759ba2f6e3634
ImilkCoffee <529301328@qq.com>
2019-03-27 10:31:00 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <router-link to='/input'>计数器</router-link>
+   <router-link to='/play'>音乐播放器</router-link> 
+    <router-link to='/weather'>天气查询</router-link>  
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  
+}
+</script>
+
+<style>
+
+</style>
